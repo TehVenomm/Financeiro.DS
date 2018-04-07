3b739b02f47a716773f6737fc03bf5871d4ff463
Guilherme Senes <36831112+guilhermesenes@users.noreply.github.com>
2018-04-06 21:08:50 -0300

    BD Populado

--- SQL/financeiro_bd.sql
+++ SQL/financeiro_bd.sql
@@ -1,60 +1,157 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 07-Abr-2018 às 02:08
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
-CREATE SCHEMA IF NOT EXISTS `financeiro_bd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `financeiro_bd` ;
-
--- -----------------------------------------------------
--- Table `financeiro_bd`.`categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `financeiro_bd`.`categoria` (
-  `idCategoria` INT NOT NULL AUTO_INCREMENT ,
-  `nome` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idCategoria`) )
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `financeiro_bd`.`usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `financeiro_bd`.`usuario` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT ,
-  `email` VARCHAR(200) NOT NULL ,
-  `senha` VARCHAR(32) NOT NULL ,
-  `tipoConta` BIT NOT NULL ,
-  PRIMARY KEY (`idUsuario`) )
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `financeiro_bd`
+--
+CREATE DATABASE IF NOT EXISTS `financeiro_bd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `financeiro_bd`;
 
--- -----------------------------------------------------
--- Table `financeiro_bd`.`lancamento`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `financeiro_bd`.`lancamento` (
-  `idLancamento` INT NOT NULL AUTO_INCREMENT ,
-  `valor` DOUBLE NOT NULL ,
-  `dataEfetuada` VARCHAR(45) NOT NULL ,
-  `descricao` INT NOT NULL ,
-  `tipo` CHAR NOT NULL ,
-  `idCategoria_Lancamento` INT NOT NULL ,
-  `usuario_idUsuario` INT NOT NULL ,
-  PRIMARY KEY (`idLancamento`, `usuario_idUsuario`) ,
-  INDEX `fk_lancamento_usuario1_idx` (`usuario_idUsuario` ASC) ,
-  CONSTRAINT `idCategoria_Lancamento`
-    FOREIGN KEY (`idLancamento` )
-    REFERENCES `financeiro_bd`.`categoria` (`idCategoria` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_lancamento_usuario1`
-    FOREIGN KEY (`usuario_idUsuario` )
-    REFERENCES `financeiro_bd`.`usuario` (`idUsuario` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
-USE `financeiro_bd` ;
+--
+-- Estrutura da tabela `categoria`
+--
 
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`idCategoria`, `nome`) VALUES
+(1, 'Manutenções'),
+(2, 'Mão de obra terceiros'),
+(3, 'Transportes'),
+(4, 'Correio'),
+(5, 'Telefones fixo'),
+(6, 'Telefone celular'),
+(7, 'Mat. de limpeza'),
+(8, 'Lanches'),
+(9, 'Combustivel'),
+(10, 'Vale transporte'),
+(11, 'Estadias'),
+(12, 'Outros'),
+(13, 'Tarifas Banco'),
+(14, 'Manut. conta/cartão'),
+(15, 'Boletos'),
+(16, 'Imposto'),
+(17, 'Móveis');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lancamento`
+--
+
+CREATE TABLE `lancamento` (
+  `idLancamento` int(11) NOT NULL,
+  `valor` double NOT NULL,
+  `dataEfetuada` varchar(45) NOT NULL,
+  `descricao` int(11) NOT NULL,
+  `tipo` char(1) NOT NULL,
+  `idCategoria_Lancamento` int(11) NOT NULL,
+  `usuario_idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `tipoConta` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `email`, `senha`, `tipoConta`) VALUES
+(1, 'adm@adm.adm', '63a9f0ea7bb98050796b649e85481845', b'1'),
+(2, 'usr@usr.usr', 'f8032d5cae3de20fcec887f395ec9a6a', b'0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`);
+
+--
+-- Indexes for table `lancamento`
+--
+ALTER TABLE `lancamento`
+  ADD PRIMARY KEY (`idLancamento`,`usuario_idUsuario`),
+  ADD KEY `fk_lancamento_usuario1_idx` (`usuario_idUsuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `lancamento`
+--
+ALTER TABLE `lancamento`
+  MODIFY `idLancamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `lancamento`
+--
+ALTER TABLE `lancamento`
+  ADD CONSTRAINT `fk_lancamento_usuario1` FOREIGN KEY (`usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `idCategoria_Lancamento` FOREIGN KEY (`idLancamento`) REFERENCES `categoria` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
