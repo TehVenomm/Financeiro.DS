9783321c44735b4faf7682b435603c871dd1dc70
Guilherme Senes <36831112+guilhermesenes@users.noreply.github.com>
2018-04-06 21:43:34 -0300

    Só vai então

---
+++ cadastroDespesa.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<title>Atividade 29/03</title>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+	</head>
+	<body>
+		<header>
+			<hr>
+			<h2><center>Projeto Financeiro - Cadastro de Despesas - Braz & Senes</center></h2>
+			<hr>
+		</header>
+		<?php
+			//include "validaExistencia.php"
+		
+		?>
+
+		<div>
+			<fieldset>
+				<form action="" method="POST">
+					Valor da Despesa:
+					<input type='number' name='valor' placeholder='Insira o valor da sua despesa' required> <br>
+					Data efetuada:
+					<input type='date'   name='valor' > <br>
+					Descrição
+					<input type='number' name='valor' placeholder='Descreva a despesa' required
+				</form>
+			</fieldset>	
+		</div>
+	</body>
+</html>
